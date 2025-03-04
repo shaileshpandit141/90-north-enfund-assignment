0930389c395d7f80800841930be9caefbb488ccb
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-04 13:50:32 +0530

    docs: add a comment to explain the base url in your app urls

--- user_auth_app/urls.py
+++ user_auth_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import GoogleAuthView, GoogleCallbackView
 
+# Base url is "api/v1/auth/"
 urlpatterns = [
     path("google/", GoogleAuthView.as_view(), name="google_auth"),
     path("google/callback/", GoogleCallbackView.as_view(), name="google_auth_callback"),
