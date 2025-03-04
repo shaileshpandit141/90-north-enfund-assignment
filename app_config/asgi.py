90498ee040ff39a899afa4428b34d3fb77f32d2c
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-04 15:54:14 +0530

    feat: configure user chat app routing in app config project

--- app_config/asgi.py
+++ app_config/asgi.py
@@ -6,12 +6,13 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from user_chat_app import routing as user_chat_app_routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app_config.settings")
 
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
-        # "websocket": URLRouter(),
+        "websocket": URLRouter(user_chat_app_routing.websocket_urlpatterns),
     }
 )
