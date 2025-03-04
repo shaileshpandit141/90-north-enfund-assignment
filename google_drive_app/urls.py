0593e57a93bda0b005e28409cd13f6499575e611
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-03-04 13:45:09 +0530

    feat: update google drive app urls as for its views

--- google_drive_app/urls.py
+++ google_drive_app/urls.py
@@ -1,3 +1,18 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (
+    GoogleDriveFileDetailView,
+    GoogleDriveFileListView,
+    GoogleDriveFileUploadView,
+)
+
+# Base url is "api/v1/google/drive/"
+urlpatterns = [
+    path("upload/", GoogleDriveFileUploadView.as_view(), name="drive_file_upload"),
+    path("list/", GoogleDriveFileListView.as_view(), name="drive_file_list"),
+    path(
+        "list/<str:file_id>/",
+        GoogleDriveFileDetailView.as_view(),
+        name="drive_file_detail",
+    ),
+]
